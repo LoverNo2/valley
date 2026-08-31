c8d93c178c9a7c67d04c6d1125fb5be26b49a385
Arca200 <wu.xinhai@outlook.com>
2026-08-31 17:32:33 +0800

    更新游戏场景和玩家动画，添加新的动画状态和调整输入处理逻辑

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,20 @@ var direction = Vector2.ZERO
 
 
 func _physics_process(_delta: float) -> void:
-	direction = Input.get_vector("left", "right", "up", "down")
+	_get_input()
 	velocity = direction * SPEED
+	animation()
 	move_and_slide()
+
+
+func _get_input() -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
+
+
+func animation() -> void:
+	if direction != Vector2.ZERO:
+		var dir: Vector2 = Vector2(round(direction.x), round(direction.y))
+		$AnimationTree.set("parameters/StateMachine/walk/blend_position", dir)
+	else:
+		var dir: Vector2 = Vector2(round(direction.x), round(direction.y))
+		$AnimationTree.set("parameters/StateMachine/idle/blend_position", dir)
