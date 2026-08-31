f6a1cda3993fb275cfa9fee35b60c83091620608
wuxinhai <wu.xinhai@outlook.com>
2026-08-31 20:54:12 +0800

    更新玩家脚本以支持工具切换功能，调整动画状态机位置和逻辑

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,20 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 100
 var direction = Vector2.ZERO
 
+
+enum Tools {
+	HOE,
+	AXE,
+	WATER
+}
+var current_tool = Tools.HOE
+
+var walk_state_machine: AnimationNodeStateMachinePlayback
+
+func _ready() -> void:
+	walk_state_machine = $AnimationTree.get("parameters/StateMachine/playback")
 
 func _physics_process(_delta: float) -> void:
 	_get_input()
@@ -14,11 +26,16 @@ func _physics_process(_delta: float) -> void:
 func _get_input() -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
 
+	if Input.is_action_just_pressed("tool_pre") || Input.is_action_just_pressed("tool_next"):
+		var tool_delta = 1 if Input.is_action_just_pressed("tool_next") else -1
+		current_tool = posmod(current_tool + tool_delta, Tools.size()) as Tools
+
 
 func animation() -> void:
-	if direction != Vector2.ZERO:
-		var dir: Vector2 = Vector2(round(direction.x), round(direction.y))
-		$AnimationTree.set("parameters/StateMachine/walk/blend_position", dir)
+	if direction:
+		var direction2 = Vector2i(roundi(direction.x), roundi(direction.y))
+		walk_state_machine.travel("walk")
+		$AnimationTree.set("parameters/StateMachine/walk/blend_position", direction2)
+		$AnimationTree.set("parameters/StateMachine/idle/blend_position", direction2)
 	else:
-		var dir: Vector2 = Vector2(round(direction.x), round(direction.y))
-		$AnimationTree.set("parameters/StateMachine/idle/blend_position", dir)
+		walk_state_machine.travel("idle")
